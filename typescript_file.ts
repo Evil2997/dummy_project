8b1d861e755e95972377377875de20001b248bd1
Evil2997 <dima.molchanow2012@gmail.com>
2024-11-05 08:14:58 +0200

    Update

--- typescript_file.ts
+++ typescript_file.ts
@@ -8,3 +8,12 @@ const apiServer = "192.168.1.2"; // IP for API server
 let dbHost = "10.0.0.2"; // Database IP
 // Backup server IP: 172.16.0.2
 
+const hosts = {
+  database_host: "192.168.1.1", // This is the database server IP
+  app_host: "10.0.0.1",
+  // Another server IP is 172.16.0.1
+  app_host_2: "host 2 by // ip: 114.15.0.1",
+  app_host_4: "10.0.4.1 host 2 by ip",
+  app_host_3: ": 10.2.0.12",
+  app_host_41: ": 11.0.0.172 host 2 by ip"
+};
