0accd3de87b3e3e1e81ed764ab408b7a5ee6e95f
Evil2997 <dima.molchanow2012@gmail.com>
2025-01-03 17:54:30 +0200

    Update

--- smart_module/final_file.py
+++ smart_module/final_file.py
@@ -3,24 +3,21 @@ from src.features.string_concatenation import nested_variable_string_1 as string
 from src.utils.comma_concatenation import nested_variable_comma_1 as comma_var_1
 from src.formatting.format_method import nested_variable_format_1 as format_var_1
 import src.formatting.percent_format as percent_format_module
-import src.formatting
 
-# Переопределение значения через input()
 nested_variable_main_1 = input()
 
-# Переопределение значения через импорт (string_var_1)
-# False - значение теперь импортируется
 nested_variable_main_2 = string_var_1
 
-# Переопределение значения через импорт (comma_var_1)
-# False - значение теперь импортируется
 nested_variable_main_3 = comma_var_1
-
-# Использование форматирования из percent_format_module
-# True used_in_percent
 nested_variable_main_4 = percent_format_module.nested_variable_percent_1
 
-# import src.formatting.percent_format as percent_format_module
 nested_variable = percent_format_module.nested_variable_percent_2
-nested_variable__NEW = f'my current_var is: {percent_format_module.nested_variable_percent_3}'
-variable_with_big_import_line = formatting
+
+
+# nested_variable__NEW = f'my current_var is: {percent_format_module.nested_variable_percent_3}'
+
+
+import path4.path3.path3_module1
+
+variable_with_big_import_line = path4.path3.path3_module1.variable_4
+
