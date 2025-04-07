cdf3a412f2bd080ec56c9a8c1d9bcdb27ff5808e
Evil2997 <dima.molchanow2012@gmail.com>
2025-04-07 12:09:49 +0300

    Update

--- smart_module/test_sample.py
+++ smart_module/test_sample.py
@@ -1,7 +1,7 @@
 # test_sample.py
 
 TARGET_VAR = input()
-
+from project.SubDir.exotic import current_var
 
 def test_function():
     # Прямое использование глобальной переменной
@@ -13,20 +13,23 @@ def test_function():
     # Вложенная функция, использующая TARGET_VAR
     def inner_function():
         # Использование TARGET_VAR внутри вложенной функции
-        modified = TARGET_VAR.upper()
+        def third_lvl():
+            # a = current_var
+            return current_var
+        modified = TARGET_VAR.upper() + third_lvl()
         return f"Inner: {modified}"
 
     # Lambda-выражение, использующее TARGET_VAR
     lambda_result = (lambda suffix: TARGET_VAR + suffix)("!!!")
-
+    result = eval(TARGET_VAR)
     # Результаты работы вложенной функции и lambda
     inner_result = inner_function()
     print("Inner function result:", inner_result)
     print("Lambda result:", lambda_result)
-
-    return new_message
+    eval_result = result
+    return new_message, eval_result
 
 
 if __name__ == '__main__':
-    final_message = test_function()
+    final_message, result_with_eval = test_function()
     print("Final message:", final_message)
